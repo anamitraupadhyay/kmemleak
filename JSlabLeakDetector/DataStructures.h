c760734e6c80f4ca60399f730e59204107bb5bd8
anamitraupadhyay <anamitraupadhyay@gmail.com>
2025-11-13 13:57:21 +0000

    updated the gitignore and add preliminary data design for the proc directories files

--- JSlabLeakDetector/DataStructures.h
+++ JSlabLeakDetector/DataStructures.h
@@ -6,19 +6,22 @@
 #define KMEMLEAK_DATASTRUCTURES_H
 
 typedef struct {
-    //
+  //datas made available by 'cat /proc/slabinfo'
+  struct list *list;
 }slabinfo;
 
 typedef struct {
-    //
+  //datas made available by 'cat /proc/vmstat'
+  struct list *list;
 }vmstat;
 
 typedef struct {
-    //
+  //datas made available by 'cat /proc/buddyinfo'
+  struct list *list;
 }buddyinfo;
 
 typedef struct {
-    //
+    struct list *prev, *next;
 }list;
 
 #endif //KMEMLEAK_DATASTRUCTURES_H
